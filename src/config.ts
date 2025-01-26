e8985328341eef4649de739870009aa4ac611af2
vientorepublic <markgod0419@gmail.com>
2025-01-26 16:02:13 +0900

    feat: add melon popular/trending keywords

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,6 @@ export enum Config {
   CHART_URL = '/chart/index.htm',
   NEW_MUSIC_URL = '/new/index.htm',
   LIKE_CNT_JSON = '/commonlike/getSongLike.json',
+  KEYWORD_CHART_URL = '/search/side/keywordChart.htm',
   USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
 }
--- src/melon.ts
+++ src/melon.ts
@@ -32,6 +32,79 @@ export interface INewMusicData extends ISongData {
   albumImg: string;
 }
 
+export interface IKeyword {
+  rank: number;
+  keyword: string;
+  rankChanges: string;
+}
+
+export interface IKeywordChart {
+  trending: IKeyword[];
+  popular: IKeyword[];
+}
+
+export class MelonKeywords {
+  private http: HTTP;
+  constructor() {
+    this.http = new HTTP();
+  }
+
+  public async parseTable(html: string): Promise<IKeywordChart> {
+    const $ = cheerio.load(html);
+    const body = $('body');
+    const content = body.find('div#side_conts > div.side_cont');
+    const data: IKeywordChart = {
+      trending: [],
+      popular: [],
+    };
+    const trending = content.find('div:nth-child(2)');
+    const trendingTable = trending.find('ul');
+    trendingTable.map((i, el) => {
+      const li = $(el).find('li');
+      li.map((i, el) => {
+        const rank = i + 1;
+        const keyword = $(el)
+          .find('div.wrap > div.cntt > div.ellipsis > a')
+          .text()
+          .trim();
+        const rankChanges = $(el).find('div.rank > span').attr('title').trim();
+        data.trending.push({
+          rank,
+          keyword,
+          rankChanges,
+        });
+      });
+    });
+    const popular = content.find('div:nth-child(4)');
+    const popularTable = popular.find('ul');
+    popularTable.map((i, el) => {
+      const li = $(el).find('li');
+      li.map((i, el) => {
+        const rank = i + 1;
+        const keyword = $(el)
+          .find('div.wrap > div.cntt > div.ellipsis > a')
+          .text()
+          .trim();
+        const rankChanges = $(el).find('div.rank > span').attr('title').trim();
+        data.popular.push({
+          rank,
+          keyword,
+          rankChanges,
+        });
+      });
+    });
+    return data;
+  }
+
+  public async getKeywords(): Promise<IKeywordChart> {
+    const params = new URLSearchParams();
+    params.append('query', '');
+    const html = await this.http.getHTML(Config.KEYWORD_CHART_URL, params);
+    const data = await this.parseTable(html);
+    return data;
+  }
+}
+
 export class MelonSearch {
   private http: HTTP;
   constructor() {
